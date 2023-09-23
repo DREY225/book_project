e0e501f2de2b9433cee1f35abea4ff38afa32b94
Régis KOFFI <landrykoffi@cesag.edu.sn>
2023-09-23 10:04:27 +0000

    Création du model Book

--- BOOK_PROJECT/book/models.py
+++ BOOK_PROJECT/book/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    
+    book_type = [
+        ('RM','Roman'),
+        ('NV','Nouvelle'),
+        ('PT','Pièce de théatre'),
+        ('PS','Poésie')
+    ]
+
+    title = models.CharField(max_length = 100)
+    description = models.CharField(max_length = 200)
+    book_gender = models.CharField(max_length=2, choices = book_type)
+    
+    
+    class Meta:
+        db_table = 'livres'
+        managed = True
+        verbose_name = 'livre'
+        verbose_name_plural = 'livres'
